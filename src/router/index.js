062cb95fd6571362e61b6dc8e73f3e2c0e4749f9
shiki <kamiacgxu@gmail.com>
2024-05-08 09:36:35 +0800

    svve

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router';
+import { createRouter, createWebHashHistory } from 'vue-router';
 
 const Home = () => import(/* webpackChunkName: "Home" */ '@/views/Home/HomeIndex');
 // 油价查询
@@ -16,7 +16,7 @@ const routes = [
 ];
 
 const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHashHistory(),
   routes,
   scrollBehavior() {
     // 始终滚动到顶部
